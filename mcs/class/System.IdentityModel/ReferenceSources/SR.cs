11abb9d89521ba7cbe7977df0dea471c67aada75
Atsushi Eno <atsushieno@gmail.com>
2015-05-01 15:49:33 +0900

    [IdentityModel] add common SR.cs.

--- mcs/class/System.IdentityModel/ReferenceSources/SR.cs
+++ mcs/class/System.IdentityModel/ReferenceSources/SR.cs
@@ -1,35 +1,5 @@
 using System.Globalization;
 
-#region "copy of ../../build/common/SR.cs"
-
-//namespace System.IdentityModel
-//{
-	static partial class SR 
-	{
-
-        internal static string GetString(string name, params object[] args)
-        {
-                return GetString (CultureInfo.InvariantCulture, name, args);
-        }
-
-        internal static string GetString(CultureInfo culture, string name, params object[] args) {
-                return string.Format (culture, name, args);
-        }
-
-        internal static string GetString(string name)
-        {
-                return name;
-        }
-
-        internal static string GetString(CultureInfo culture, string name)
-        {
-                return name;
-        }
-	}
-//}
-
-#endregion
-
 #region "retrieved string resources"
 
 //namespace System.IdentityModel {
